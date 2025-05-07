e1a7d94167ebb8e151f01ccabf1eb7fbc2a04515
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2025-05-07 20:57:25 +0300

    chore: remove redundant nvim plugins

--- nvim/lua/josean/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreview", "MarkdownPreviewToggle" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-}
