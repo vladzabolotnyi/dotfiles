00863aa2b38aa867be27a0854d1711aa262c05dd
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2025-07-15 11:15:57 +0300

    plugins: update trouble api and setup

--- config/nvim/lua/config/plugins/trouble.lua
+++ config/nvim/lua/config/plugins/trouble.lua
@@ -1,12 +1,50 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  cmd = "Trouble",
+  opts = {
+    modes = {
+      symbols = {
+        win = {
+          position = "right",
+          size = 0.5,
+        },
+        focus = false,
+      },
+    },
+  },
   keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Open/Close Workspace Trouble",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Open/Close File Trouble",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Open/Close Symbols",
+    },
+    -- INFO: Figure out the usage or remove completely
+    -- {
+    --   "<leader>cl",
+    --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    --   desc = "LSP Definitions / references / ... (Trouble)",
+    -- },
+    -- {
+    --   "<leader>xL",
+    --   "<cmd>Trouble loclist toggle<cr>",
+    --   desc = "Location List (Trouble)",
+    -- },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in Trouble" },
   },
 }
